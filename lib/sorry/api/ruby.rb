17c211f48bfb8e5b7f07995d76ad0a900e495805
Robert Rawlins <robert@weboffins.com>
2016-02-18 13:23:53 +0000

    Skeleton gem content generated by Bunlder.

---
+++ lib/sorry/api/ruby.rb
@@ -0,0 +1,9 @@
+require "sorry/api/ruby/version"
+
+module Sorry
+  module Api
+    module Ruby
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/sorry/api/ruby/version.rb
@@ -0,0 +1,7 @@
+module Sorry
+  module Api
+    module Ruby
+      VERSION = "0.0.1"
+    end
+  end
+end
